036681acacb8d6177b06629ec000cf36771190b7
Cem Tanrikut <cemtanrikut@gmail.com>
2024-07-02 15:07:18 +0200

    add operation class with enum auto

--- app/channable_assignment_python.py
+++ app/channable_assignment_python.py
@@ -3,6 +3,11 @@ import csv
 from typing import Iterator, Any, Dict, Tuple
 from enum import Enum, auto
 
+class Operation(Enum):
+    CREATE = auto()
+    UPDATE = auto()
+    DELETE = auto()
+
 class ProductStreamProcessor(metaclass=abc.ABCMeta):
     def __init__(self, path_to_before_csv: str, path_to_after_csv: str) -> None:
         self.path_to_before_csv = path_to_before_csv
@@ -22,3 +27,19 @@ class ProductDiffer(ProductStreamProcessor):
             for row in reader:
                 products[row['id']] = row
         return products
+    def main(self) -> Iterator[Tuple[Operation, str, Dict[str, Any] | None]]:
+        before_products = self.read_csv(self.path_to_before_csv)
+        after_products = self.read_csv(self.path_to_after_csv)
+
+        before_ids = set(before_products.keys())
+        after_ids = set(after_products.keys())
+
+        for product_id in after_ids - before_ids:
+            yield (Operation.CREATE, product_id, after_products[product_id])
+
+        for product_id in before_ids - after_ids:
+            yield (Operation.DELETE, product_id, None)
+
+        for product_id in before_ids & after_ids:
+            if before_products[product_id] != after_products[product_id]:
+                yield (Operation.UPDATE, product_id, after_products[product_id])
