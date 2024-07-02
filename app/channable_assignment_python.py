720dd264a57c555112e528b855ef45871facb57f
Cem Tanrikut <cemtanrikut@gmail.com>
2024-07-02 15:05:47 +0200

    add product differ and csv reader

--- app/channable_assignment_python.py
+++ app/channable_assignment_python.py
@@ -11,3 +11,14 @@ class ProductStreamProcessor(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def main(self) -> Iterator[Tuple[Operation, str, Dict[str, Any] | None]]:
         pass
+        # The reason why I do not pass the main method and implement it in the 
+        # ProductDiffer class is to ensure the correct use of abstract classes and methods.
+
+class ProductDiffer(ProductStreamProcessor):
+    def read_csv(self, path: str) -> Dict[str, Dict[str, Any]]:
+        products = {}
+        with open(path, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                products[row['id']] = row
+        return products
